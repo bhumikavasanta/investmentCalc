8be61795ca5087eb286e803f7f6306d7e4528c00
Bhumika Vasanta <bhumikavasanta2460@gmail.com>
2024-12-09 23:47:17 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import './App.css'
+import Header from "./components/Header"
+import UserInput from "./components/UserInput"
+
+const App = () => {
+
+  return (
+    <>
+      <Header />
+      <UserInput />
+    </>
+  )
+}
+
+export default App
